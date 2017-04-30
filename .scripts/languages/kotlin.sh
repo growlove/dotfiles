6dc7c16654100605b94384af76c2f74a26ad7257
Mark Chandler <mark.allen.chandler@gmail.com>
2017-04-29 22:15:14 -0400

    Make args to kotlin_run pass through to jar run

--- .scripts/languages/kotlin.sh
+++ .scripts/languages/kotlin.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
 function kotlin_run() {
+  ARGS="${*##*.kt }"
   KT_FILE=$1
   JAR_FILE="${KT_FILE%.kt}.jar"
 
   kotlinc $KT_FILE -include-runtime -d $JAR_FILE
   if ((! $? > 0)); then
-    java -jar $JAR_FILE
+    java -jar $JAR_FILE $(echo $ARGS)
     rm $JAR_FILE
   fi
 }
